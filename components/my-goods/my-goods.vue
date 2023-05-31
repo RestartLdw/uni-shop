e666251f5f847df3b17d6b20b0320afad7ecee70
liudiwen <1164678512@qq.com>
2023-05-31 09:06:45 +0800

    goods_list

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,88 @@
+<template>
+    <view>
+        <view class="goods-item">
+            <view class="left-goods-box">
+                <image :src="goods.goods_small_logo || defaultPic" class="goods-image" ></image>
+            </view>
+            
+            <view class="right-goods-box">
+                <view class="goods-name">{{goods.goods_name}}</view>
+                <view class="goods-info">
+                    
+                    <view class="goods-price">￥{{goods.goods_price | fixMoney}}</view>
+                    <view class="goods-cart" @click="addToCart(goods.goods_id)">
+                        <view v-if="!goods.addToCartNum">
+                            <uni-icons type="cart" size="24" color="#c00000"></uni-icons>
+                        </view>
+                        <view v-else>
+                            <uni-icons type="cart-filled" size="24" color="#c00000"></uni-icons>
+                        </view>
+                    </view>
+                    
+                </view>
+            </view>  
+        </view>
+    </view>
+</template>
+
+<script>
+    export default {
+        name:"my-goods",
+        //props 接收外界信息
+        props: {
+            goods: {
+                type: Object,
+                default: {}
+            }
+        },
+        data() {
+            return {
+                defaultPic: "http://image4.suning.cn/uimg/b2c/newcatentries/0000000000-000000000606013705_1_400x400.jpg"    
+            };
+        },
+            
+        filters: {
+            fixMoney(num) {
+                return Number(num).toFixed(2)
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss">
+.goods-item {
+        display: flex;
+        border-bottom: 1px solid #efefef;
+        
+        .left-goods-box {
+            margin: 5px;
+            .goods-image {
+                width: 100px;
+                height: 100px;
+                display: block; //防止图片下面间隙
+            }
+        }
+        .right-goods-box {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            width: 600px;
+            
+            .goods-name {
+                font-size: 14px;
+                padding-top: 15px;
+                color: #2d3a42;
+                justify-content: center;
+                align-items: center;
+            }
+            .goods-info {
+                display: flex;
+                justify-content: space-between;
+                font-size: 16px;
+                color: #c00000;
+                padding-bottom: 15px;
+            }
+        }
+    }
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,11 @@
 <template>
     <view>
-        goods_list
+        <view class="goods-list">
+            <navigator v-for="(item, i) in goodsList" :key="i" :url="'/subpkg/goods_detail/goods_detail?good_id=' + item.goods_id">
+                <my-goods :goods="item"></my-goods>
+            </navigator> 
+        </view>
+        
     </view>
 </template>
 
@@ -8,12 +13,77 @@
     export default {
         data() {
             return {
+                queryObj: {
+                    // 搜索词
+                    query: '',
+                    // 商品分类
+                    cid: '',
+                    pagenum: 1,
+                    pagesize: 10
+                },
                 
+                // 商品列表
+                goodsList: [],
+                // 商品列表总量
+                total: 0,
+                // 默认图片
+                defaultPic: "http://image4.suning.cn/uimg/b2c/newcatentries/0000000000-000000000606013705_1_400x400.jpg",
+                isLoading: false
             };
+        },
+        
+        onLoad(options) {
+            // 页面参数转存到queryObj
+            console.log(options)
+            this.queryObj.query = options.query || ''
+            this.queryObj.cid = options.cid || ''
+            this.getGoodsList()
+        }, 
+        
+        //下拉刷新
+        onPullDownRefresh() {
+            this.queryObj.pagenum = 1
+            this.total = 0
+            this.goodsList = []
+            this.isLoading = false
+            this.getGoodsList(() => uni.stopPullDownRefresh())
+        },
+        
+        //上拉触底刷新
+        onReachBottom() {
+            if (this.isLoading) {
+                return
+            }
+            if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) {
+                console.log("this.pagenum * this.pagesize >= this.total")
+                return
+            }
+            this.queryObj.pagenum += 1
+            
+            this.getGoodsList()
+        },
+        
+        methods: {
+            
+            async getGoodsList(cb) {
+                
+                this.isLoading = true
+                const {data: res} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+                if (res.meta.status != 200) {
+                    return uni.$showMsg()
+                }
+                cb && cb()
+                this.goodsList = [...this.goodsList, ...res.message.goods]
+                
+                this.total = res.message.total
+                this.isLoading = false
+            },
         }
     }
 </script>
 
 <style lang="scss">
-
+.goods-list {
+    margin: 0 5px 5px 5px;
+}
 </style>
